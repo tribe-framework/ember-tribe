4276809ac96f2c3f1a4f8119c7d5a9cda4c7f668
akshay / tw <akshay.wildfire@pm.me>
2022-03-10 04:09:52 +0530

    ENV instead of config

--- blueprints/ember-tribe/files/app/adapters/application.js
+++ blueprints/ember-tribe/files/app/adapters/application.js
@@ -1,9 +1,9 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
-import config from '<%= dasherizedPackageName %>/config/environment';
+import ENV from '<%= dasherizedPackageName %>/config/environment';
 import { underscore } from '@ember/string';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
-  host = config.TribeENV.API_URL;
+  host = ENV.TribeENV.API_URL;
 
   pathForType(type) {
     return underscore(type);
