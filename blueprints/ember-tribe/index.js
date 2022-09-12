f04218fabb3fdcf592aa5d38277d3938113e25a3
akshay.wildfire / tw <akshay.wildfire@pm.me>
2022-09-13 01:49:59 +0530

    ember-truth-helpers, ember-composable-helpers, ember-math-helpers, ember-cli-string-helpers

--- blueprints/ember-tribe/index.js
+++ blueprints/ember-tribe/index.js
@@ -5,7 +5,7 @@ module.exports = {
 
   afterInstall() {
     return this.addAddonsToProject({
-      packages: [{ name: 'ember-cli-sass' }, { name: 'ember-cli-favicon' }, { name: '@ember/optional-features' }, { name: '@ember/render-modifiers' }, { name: 'ember-modifier' }],
+      packages: [{ name: 'ember-cli-sass' }, { name: 'ember-cli-favicon' }, { name: '@ember/optional-features' }, { name: '@ember/render-modifiers' }, { name: 'ember-modifier' }, { name: 'ember-modifier' }, { name: 'ember-composable-helpers' }, { name: 'ember-truth-helpers' }, { name: 'ember-math-helpers' }, { name: 'ember-cli-string-helpers' }],
     }).then(() => {
       return this.addPackagesToProject([
         { name: 'bootstrap' },
