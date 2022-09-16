f89bcc37a27a5013929823c5569bbcaea7783e37
akshay.wildfire / tw <akshay.wildfire@pm.me>
2022-09-17 00:28:05 +0530

    fontawesome as npm

--- blueprints/ember-tribe/index.js
+++ blueprints/ember-tribe/index.js
@@ -5,10 +5,21 @@ module.exports = {
 
   afterInstall() {
     return this.addAddonsToProject({
-      packages: [{ name: 'ember-cli-sass' }, { name: 'ember-cli-favicon' }, { name: '@ember/optional-features' }, { name: '@ember/render-modifiers' }, { name: 'ember-modifier' }, { name: 'ember-modifier' }, { name: 'ember-composable-helpers' }, { name: 'ember-truth-helpers' }, { name: 'ember-math-helpers' }, { name: 'ember-cli-string-helpers' }],
+      packages: [
+      { name: 'ember-cli-sass' }, 
+      { name: 'ember-cli-favicon' }, 
+      { name: '@ember/optional-features' }, 
+      { name: '@ember/render-modifiers' }, 
+      { name: 'ember-modifier' }, 
+      { name: 'ember-modifier' }, 
+      { name: 'ember-composable-helpers' }, 
+      { name: 'ember-truth-helpers' }, 
+      { name: 'ember-math-helpers' }, 
+      { name: 'ember-cli-string-helpers' }, ],
     }).then(() => {
       return this.addPackagesToProject([
         { name: 'bootstrap' },
+        { name: '@fortawesome/fontawesome-pro' },
       ]);
     });
   },
