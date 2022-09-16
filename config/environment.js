711500dfb3c911275c3a29c140a79291f8cebe51
akshay.wildfire / tw <akshay.wildfire@pm.me>
2022-09-17 00:28:25 +0530

    environment variable TribeENV

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,10 @@
 'use strict';
 
-module.exports = function (/* environment, appConfig */) {
-  return {};
+module.exports = function (environment) {
+  ENV.TribeENV = {
+    /* Uncomment the line below, define the API URL */
+    //API_URL: 'https://tribe/api/url/',
+  };
+
+  return ENV;
 };
