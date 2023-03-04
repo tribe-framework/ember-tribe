182ee1c489d912a45cac723fe77760ab1e943db8
cityowl / jigmet <cityowl@hey.com>
2023-03-04 15:10:25 +0530

    handling model names with underscore

--- blueprints/ember-tribe/files/sync-types.php
+++ blueprints/ember-tribe/files/sync-types.php
@@ -1,12 +1,14 @@
 <?php
 $types = json_decode(file_get_contents('../../config/types.json'), true);
 
-$commands = "rm app/models -R; ";
-$commands = "rm tests/unit/models -R; ";
+$commands = "sudo rm app/models -R; ";
+$commands = "sudo rm tests/unit/models -R; ";
 $commands = "mkdir app/models; ";
 
 foreach (array_keys($types) as $type) {
-	$commands .= "echo \"import Model, { attr } from '@ember-data/model'; export default class ".ucfirst($type)."Model extends Model { @attr modules; }\" >> app/models/".$type.".js; ";
+	$type_hyphen = str_replace('_', '-', $type);
+	$type_ucwords = str_replace(' ', '', ucwords(str_replace('_', ' ', $type)));
+	$commands .= "echo \"import Model, { attr } from '@ember-data/model'; export default class ".$type_ucwords."Model extends Model { @attr modules; }\" >> app/models/".$type_hyphen.".js; ";
 }
 
 exec($commands);
