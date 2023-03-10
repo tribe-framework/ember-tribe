b02135a06e310bb9be407585d020677ff7535596
cityowl / jigmet <cityowl@hey.com>
2023-03-10 13:09:14 +0530

    replacement instead of appending

--- blueprints/ember-tribe/files/sync-types.php
+++ blueprints/ember-tribe/files/sync-types.php
@@ -8,7 +8,7 @@ $commands = "mkdir app/models; ";
 foreach (array_keys($types) as $type) {
 	$type_hyphen = str_replace('_', '-', $type);
 	$type_ucwords = str_replace(' ', '', ucwords(str_replace('_', ' ', $type)));
-	$commands .= "echo \"import Model, { attr } from '@ember-data/model'; export default class ".$type_ucwords."Model extends Model { @attr modules; }\" >> app/models/".$type_hyphen.".js; ";
+	$commands .= "echo \"import Model, { attr } from '@ember-data/model'; export default class ".$type_ucwords."Model extends Model { @attr modules; }\" > app/models/".$type_hyphen.".js; ";
 }
 
 exec($commands);
