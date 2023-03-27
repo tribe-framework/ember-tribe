9578616706ca4e3ed892eab2dec45094fb7de427
cityowl / jigmet <cityowl@hey.com>
2023-03-27 17:39:07 +0530

    type/model constraints in front-end interfaces

--- blueprints/ember-tribe/files/sync-types.php
+++ blueprints/ember-tribe/files/sync-types.php
@@ -6,9 +6,14 @@ $commands = "sudo rm tests/unit/models -R; ";
 $commands = "mkdir app/models; ";
 
 foreach (array_keys($types) as $type) {
-	$type_hyphen = str_replace('_', '-', $type);
-	$type_ucwords = str_replace(' ', '', ucwords(str_replace('_', ' ', $type)));
-	$commands .= "echo \"import Model, { attr } from '@ember-data/model'; export default class ".$type_ucwords."Model extends Model { @attr modules; }\" > app/models/".$type_hyphen.".js; ";
+
+	if (in_array($type, ($types['webapp']['interface_urls'][basename(dirname(__FILE__))]['types'] ?? array_keys($types)))) {
+
+		$type_hyphen = str_replace('_', '-', $type);
+		$type_ucwords = str_replace(' ', '', ucwords(str_replace('_', ' ', $type)));
+		$commands .= "echo \"import Model, { attr } from '@ember-data/model'; export default class ".$type_ucwords."Model extends Model { @attr modules; }\" > app/models/".$type_hyphen.".js; ";
+		
+	}
 }
 
 exec($commands);
