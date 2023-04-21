2db3e6ec1218c54617f1d3b77f6b395ce3e3e13e
akshay.wildfire / tw <akshay.wildfire@pm.me>
2023-04-22 00:18:42 +0530

    updated serialiser

--- blueprints/ember-tribe/files/app/serializers/application.js
+++ blueprints/ember-tribe/files/app/serializers/application.js
@@ -2,13 +2,11 @@ import JSONAPISerializer from '@ember-data/serializer/json-api';
 import { underscore } from '@ember/string';
 
 export default class ApplicationSerializer extends JSONAPISerializer {
-
   keyForAttribute(attr) {
     return underscore(attr);
   }
 
   payloadKeyFromModelName(key) {
-    return key;
+    return underscore(key);
   }
-
 }
