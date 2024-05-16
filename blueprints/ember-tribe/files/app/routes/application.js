e020acf2301aab19b13945837c387b913c56adf8
cityowl / moti <cityowl@hey.com>
2024-05-16 08:12:58 +0530

    types.json as a service

--- blueprints/ember-tribe/files/app/routes/application.js
+++ blueprints/ember-tribe/files/app/routes/application.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
 import * as bootstrap from 'bootstrap';
+import { service } from '@ember/service';
 
-export default class ApplicationRoute extends Route {}
+export default class ApplicationRoute extends Route {
+  /*
+  @service types;
+
+  async beforeModel() {
+    await this.types.fetchAgain();
+  }
+  */
+}
