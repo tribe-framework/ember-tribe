15a186f6d0263bc793926aff35c04677ec8ae800
cityowl / moti <cityowl@hey.com>
2024-11-24 18:16:44 +0530

    TRIBE_API_KEY

--- config/environment.js
+++ config/environment.js
@@ -2,12 +2,11 @@
 
 module.exports = function (/* environment, appConfig */) {
   let ENV = {
-
     //Wildfire Tribe config for EmberJS
     TribeENV: {
       API_URL: process.env.TRIBE_API_URL,
+      API_KEY: process.env.TRIBE_API_KEY,
     },
-
   };
 
   return ENV;
